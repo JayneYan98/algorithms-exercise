280007b3ba98060b3d2cc3f65736973bda536f96
jayne <jayneyan@yeah.net>
2021-01-21 15:55:52 +0800

    feat: add insertionSort

--- 10-1.sorting-searching-algorithms.js
+++ 10-1.sorting-searching-algorithms.js
@@ -43,4 +43,19 @@ class ArrayList {
             }
         }
     }
+
+    insertionSort = () => {
+        const length = this.array.length
+        let j
+        let temp
+        for (let i = 1; i < length; i++) {
+            j = i
+            temp = this.array[i]
+            while (j > 0 && this.array[j - 1] > temp) {
+                this.array[j] = this.array[j - 1]
+                j--
+            }
+            this.array[j] = temp
+        }
+    }
 }
