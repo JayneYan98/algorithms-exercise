52df37655131f013262aea9ba4ec6da98c15df88
jayne <jayneyan@yeah.net>
2021-01-21 14:52:08 +0800

    fix: fix bubble sort

--- 1.bubble-sort.js
+++ 1.bubble-sort.js
@@ -4,7 +4,7 @@ export default (arr = [2, 4, 5, 1, 9, 8]) => {
     for (let i = 0; i < arr.length - 1; i++) {
         count++;
         let flag = true;
-        for (j = 0; j < arr.length - 1 - i; j++) {
+        for (let j = 0; j < arr.length - 1 - i; j++) {
             if (arr[j] > arr[j + 1]) {
                 temp = arr[j];
                 arr[j] = arr[j + 1];
